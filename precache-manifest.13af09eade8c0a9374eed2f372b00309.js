884e510f80f22b57f34263fb89f152838397fda7
CodingIsFood <ayoodedere@gmail.com>
2019-02-24 19:24:46 +0100

    Updates

---
+++ precache-manifest.13af09eade8c0a9374eed2f372b00309.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfa08dad2010629d70e8",
+    "url": "/todolistApp/static/css/main.4a432148.chunk.css"
+  },
+  {
+    "revision": "dfa08dad2010629d70e8",
+    "url": "/todolistApp/static/js/main.dfa08dad.chunk.js"
+  },
+  {
+    "revision": "05fa5dd55f4be9d44d5a",
+    "url": "/todolistApp/static/js/runtime~main.05fa5dd5.js"
+  },
+  {
+    "revision": "45d506bafcea1b6b22be",
+    "url": "/todolistApp/static/js/2.45d506ba.chunk.js"
+  },
+  {
+    "revision": "8a329460b123ad630d8a466c739a1cb6",
+    "url": "/todolistApp/index.html"
+  }
+];
